614aed93da719f083a1100c2ca4aa0596cd5688b
Semyon Obukhov <56957006+ObukhovSA@users.noreply.github.com>
2022-08-04 21:18:46 +0300

    request auth

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) =>{
     res.send('Holla bolla');
+});
+
+ // Запрос на авторизацию
+app.post('/auth/login', (req, res) => { 
+    console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullname: 'Egor Alexandrovich',
+    }, 
+    'matic', 
+    );
+
+    res.json({
+        success: true,
+        token,
+    });
+
 });
 
 app.listen(5555, (err) => {
