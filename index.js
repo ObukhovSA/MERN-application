9b6630411de3a5095bc28885b70c35c3ada4e9e1
Semyon Obukhov <56957006+ObukhovSA@users.noreply.github.com>
2022-08-05 11:21:50 +0300

    DB migration
    Внедрение MongoDB cloud в информационную систему

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+    .connect('mongodb+srv://admin:admin@cluster0.iu5tnwj.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB connected'))
+    .catch((err) => console.log('error connecting to DB', err));
 
 const app = express();
 
+// Логика express для чтения JSON запросов
 app.use(express.json());
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) =>  {
     res.send('Holla bolla');
 });
 
- // Запрос на авторизацию
+// Запрос на авторизацию
 app.post('/auth/login', (req, res) => { 
     console.log(req.body);
-
-    const token = jwt.sign({
-        email: req.body.email,
-        fullname: 'Egor Alexandrovich',
-    }, 
-    'matic', 
+// Генерация токена по запросу
+    const token = jwt.sign(
+        {   
+         email: req.body.email,
+         fullname: 'Egor Alexandrovich',
+        }, 
+        'matic', // Хеширование данных по специальному ключу
     );
-
+ 
     res.json({
         success: true,
         token,
     });
 
 });
-
+// Маршрутизация базового приложения
 app.listen(5555, (err) => {
     if (err) {
         return console.log(err);
@@ -34,3 +42,4 @@ app.listen(5555, (err) => {
 
     console.log('Server working');
 });
+
