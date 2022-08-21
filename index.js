077b61a9ec385bfab8b60d6ee7e814cd76f39192
Semyon Obukhov <56957006+ObukhovSA@users.noreply.github.com>
2022-08-21 20:21:10 +0300

    MVC refactoring
    Рефакторинг index.js, создание контроллера для реализации модели MVC в будущем

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import { validationResult } from  'express-validator';
-
-
 import { registerValidation }  from './validations/auth.js';
-
-import UserModel from './models/User.js';
 import checkAuth from './utils/checkAuth.js';
+import * as UserController from './controllers/UserController';
 
 mongoose
     .connect('mongodb+srv://admin:admin@cluster0.iu5tnwj.mongodb.net/blog?retryWrites=true&w=majority',)
@@ -20,113 +14,12 @@ const app = express();
 // Логика express для чтения JSON запросов
 app.use(express.json());
 
+ // Запрос на авторизацию
+app.post('/auth/login', UserController.login);
 
-app.post('/auth/login', async (req, res) => {
-    try {
-        const user = await UserModel.findOne({ email: req.body.email });
+app.post('/auth/register', registerValidation, UserController.register) ;
 
-        if (!user) {
-           return res.status(404).json({
-             message: 'Пользователь не найден',
-           });
-        }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-        if (!isValidPass) {
-
-            return res.status(400).json({
-              message: 'Неверный логин или пароль',
-            });
-        }
-
-
-        const token = jwt.sign({
-            _id: user._id,
-        }, 
-        'secret', 
-        {
-            expiresIn: '30d',
-        })
-
-        const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-        ... userData,
-        token,
-    });
-    }   catch (err) {
-        console.log(err);
-        res.status(500).json({
-          message: 'Ошибка. Не удалось авторизоваться',
-        });
-    }
-});
-
-    // Запрос на авторизацию
-app.post('/auth/register', registerValidation, async (req, res) => { 
-    // Обработка ошибок в MongoDB 
-    try {
-        const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
-    }
-
-    // Хеширование пароля
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    // Документ создания модели пользователя
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign({
-        _id: user._id,
-    }, 
-    'secret', 
-    {
-        expiresIn: '30d',
-    })
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-        ... userData,
-        token,
-    });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({
-        message: 'Ошибка. Не удалось зарегистрироваться',
-      });
-    }
-});
-
-app.get('/auth/me', checkAuth, async (req, res) => {
-    try {
-            const user = await UserModel.findById(req.userId);
-            if (!user) {
-                return res.status(404).json({
-                    message: 'Пользователь не найден'
-                });
-            }
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json(userData);
-
-    } catch (err) {
-        console.log(err);
-      res.status(500).json({
-        message: 'Нет доступа',
-      });
-    }
-});
+app.get('/auth/me', checkAuth, UserController.getMe);
 
 // Маршрутизация базового приложения
 app.listen(5555, (err) => {
