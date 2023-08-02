5c1c32b7ac39941e8ba7e6481fe58f55834c1fe2
Semyon Obukhov <56957006+ObukhovSA@users.noreply.github.com>
2023-08-02 22:44:45 +0300

    Backend refactoring
    Оптимизация роутинга

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import multer from 'multer';
 import { registerValidation, loginValidation, postCreateValidation }  from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
+import { UserController, PostController } from './controllers/index.js';
 import handleValidationErrors from './utils/handleValidationErrors.js';
 
 
